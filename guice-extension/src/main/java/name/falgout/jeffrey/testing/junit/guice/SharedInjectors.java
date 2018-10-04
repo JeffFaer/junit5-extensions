2583e806e91b81af2ef1c3e4d91780bdbd2bcdcd
roman.ivanitsky <roman.ivanitsky@corp.mail.ru>
2018-10-04 13:06:05 +0300

    Add option to share injectors between tests.
    Close #3

---
+++ guice-extension/src/main/java/name/falgout/jeffrey/testing/junit/guice/SharedInjectors.java
@@ -0,0 +1,20 @@
+package name.falgout.jeffrey.testing.junit.guice;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
+@Inherited
+@ExtendWith(GuiceExtension.class)
+public @interface SharedInjectors {
+}
